4fd7e2a7df0394cc2e806388356dd2ce523bd59e
Sharu-Raghu <sharanya.raghunath@gmail.com>
2018-05-06 15:25:14 +1200

    Summary
    This website was created for tourists that are interested in travelling to India. This website has alot of information on tourist attractions.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tourism India
+
+##Aim: 
+This website is aimed to give information about the places to visit and tourist attractions in India.. 
+##Target Audience: 
+- Anybody interested in travelling to India. 
+- Intrest in finding out tourist attractions.
+
+##Web Technologies:
+- Adobe Dreamweaver 
+- HTML 
+- CSS
+- Javascript 
+- Adobe Photoshop 
---
+++ jscript/changecss.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+function changeCSS(cssFile) {
+ 
+        var oldlink = document.getElementsByTagName("link").item(0);
+		
+        /*create newlink and set the attributes here */
+	    var newlink = document.createElement("link")
+		newlink.setAttribute("rel", "stylesheet");
+    	newlink.setAttribute("type", "text/css");
+      	newlink.setAttribute("href", cssFile);
+
+    
+        /*replace the old link in the header with the new link */
+        document.getElementsByTagName("head").item(0).replaceChild(newlink,  oldlink);
+      }
